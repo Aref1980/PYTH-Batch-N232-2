01225aa953b468ebb3706b3616912c03cd51666e
Aref Rabbani <144184874+Aref1980@users.noreply.github.com>
2023-09-18 19:35:06 +0600

    Add files via upload

---
+++ Class-3/1-String_Intruduction.py
@@ -0,0 +1,18 @@
+
+name = "Aref Rabbani" # 0-12
+name1 = "12"
+
+print(type(name))# <class 'str'>
+print(type(name1))# <class 'str'>
+
+print(name[3:7]) # [startIndex : endIndex] f Ra
+print(name[2]) #e
+print(name[2:]) # ef Rabbani
+
+print(name[-7]) # R
+print(name[-7:]) # Rabbani
+print(name[-11:]) # ref Rabbani
+print(name[0:]) # Aref Rabbani
+print(name[-12:]) # Aref Rabbani
+print(len(name)) # 12
+
---
+++ Class-3/4-String_ Concate.py
@@ -0,0 +1,4 @@
+first_name = "Aref"
+last_name = "Rabbani"
+full_name = (first_name+ " " + last_name)
+print(full_name) # Aref Rabbani
