ce7f4fab69b52e959c187281f6f3170544596916
Aref Rabbani <144184874+Aref1980@users.noreply.github.com>
2023-09-18 19:06:12 +0600

    Add files via upload

---
+++ 1_2Variable_in_Python.py
@@ -0,0 +1,24 @@
+# Variable declare pattern;
+
+num1 = num2 = num3 = 20
+num4, num5, num6 = 50, 20, 10
+
+"""
+1-Camel Case;
+"""
+sumTwoNumber = 4+7
+
+"""
+2-Pascal Case;
+"""
+SumTwoNumber = 3+8
+
+"""
+3-Snake Case
+"""
+sum_two_number = 4+9
+Sum_Two_Number = 13
+# Both are same;
+
+First_Name = "Aref"
+last_name = "Rabbani"
