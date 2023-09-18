e3aa6a668682222cf35d5c84038bcd03aafff29c
Aref Rabbani <144184874+Aref1980@users.noreply.github.com>
2023-09-18 18:51:41 +0600

    Add files via upload

---
+++ Class-1/4_inpt_output.py
@@ -0,0 +1,41 @@
+# output
+print(20) # def print(20, end='\n') \n-> New Line
+print(10, end=" ")
+print(30)
+print(25)
+"""
+After Run
+20
+10 30
+25
+"""
+name = "Aref"
+name1 = "Rabbnai"
+print(name) # Aref
+print(name, name1) # Aref Rabbnai
+
+# input (for collect data)
+
+name = input() # always string type
+num_ = input()
+print(name)
+print(type(name)) # Check Data type
+print(num_)
+print(type(num_))
+
+"""
+inoutdate->
+aref
+10
+uotput->
+aref
+<class 'str'>
+12
+<class 'str'>
+"""
+
+# Type casting
+strTointtypecast = int(num_)
+print(strTointtypecast)
+print(type(strTointtypecast)) #<class 'int'>
+
