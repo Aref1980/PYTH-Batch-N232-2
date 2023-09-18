34f781834f2583c06fb7ced882373ede056e1ccf
Aref Rabbani <144184874+Aref1980@users.noreply.github.com>
2023-09-18 19:32:17 +0600

    Add files via upload

---
+++ Class-2/7-Identity_Operators.py
@@ -0,0 +1,18 @@
+
+"""
+    Identity is:     ' is '
+    Identity is not:    ' is not '
+"""
+x = 25
+y = 25
+z = 4
+
+print(x is y) # True
+
+print(x is not y) # Flase
+
+print(z is y) # Flase
+
+print(z is not y) # True
+
+
